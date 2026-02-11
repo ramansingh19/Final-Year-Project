ad69538c15a77c3d4aebd98867789a119f9eb2c2
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2026-02-11 15:49:10 +0530

    create app.js and start server

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,15 @@
 import express from 'express'
+import dotenv from 'dotenv'
+
+const app = express()
+
+dotenv.config()
+const PORT = process.env.PORT || 3002
+
+app.get('/', (req, res) => {
+  res.send("backend server will be start")
+})
+
+app.listen(PORT, ()=> {
+  console.log(`Server running at: http://localhost:${PORT}`)
+})
