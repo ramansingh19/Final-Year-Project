bbb1036089ec28549f0654ccfb45001074daefe9
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2026-03-08 10:03:51 +0530

    Commented out unused code in authApi.js

--- frontend/src/features/auth/authApi.js
+++ frontend/src/features/auth/authApi.js
@@ -1,5 +1,5 @@
 // import apiClient from "../../services/apiClient";
-
+// this code is not using........................
 // const authApi = {
 //   register : (data) => apiClient.post("http://localhost:3000/api/user/user-registration", data),
 //   verifyEmail : (data) => apiClient.post("http://localhost:3000/api/user/user-verification", {token})
