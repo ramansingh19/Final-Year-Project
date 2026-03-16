01a591d01babaf6e101d9204d45e0af60b2f99ea
ramansingh19 <raman.01kumar1@gmail.com>
2026-03-16 23:36:23 +0530

    s

--- backend/controllers/raman.help.js
+++ backend/controllers/raman.help.js
@@ -1,37 +1,9 @@
-console.log("Hello from raman side ");
-console.log("Hello from raman side ");
-console.log("Hello from raman side ");
-console.log("Hello from raman side ");
-console.log("Hello from raman side ");
 
-
-console.log("Hello from shiwam side ");
-console.log("Hello from shiwam side ");
-console.log("Hello from shiwam side ");
-console.log("Hello from raman side ");
-console.log("Hello from raman side ");
-console.log("Hello from raman side ");
-
-console.log("Hello from i am shivam side ");
-console.log("Hello from i am shivam side ");
-console.log("Hello from i am shivam side ");
-
-console.log("Hello from raman side ");
-console.log("Hello from raman side ");
-console.log("Hello from raman side ");
-console.log("Hello from raman side ");
-console.log("Hello from raman side ");
-console.log("Hello from raman side ");
-
-console.log("Hello from i am raman side ");
-console.log("Hello from i am raman side ");
-console.log("Hello from i am raman side ");
-console.log("Hello from i am raman side ");
-console.log("Hello from i am raman side ");
-console.log("Hello from i am raman side ");
-console.log("Hello from i am raman side ");
-console.log("Hello from i am raman side ");
-console.log("Hello from i am raman side ");
-console.log("Hello from i am raman side ");
-console.log("Hello from i am raman side ");
-
+console.log("raman ");
+console.log("raman ");
+console.log("raman ");
+console.log("raman ");
+console.log("raman ");
+console.log("raman ");
+console.log("raman ");
+console.log("raman ");
